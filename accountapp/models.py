0c3501b88278506e2ba2cf82445a8913b5c539cc
noeul1114 <parkhyongsok@naver.com>
2021-12-10 11:41:14 +0900

    Model update

--- accountapp/models.py
+++ accountapp/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
 # Create your models here.
+
+
+class NewModel(models.Model):
+    text = models.CharField(max_length=255, null=False)
+    created_at = models.DateTimeField(auto_now_add=True)
--- accountapp/views.py
+++ accountapp/views.py
@@ -2,6 +2,7 @@ from django.http import HttpResponse
 from django.shortcuts import render
 
 # Create your views here.
+from accountapp.models import NewModel
 
 
 def hello_world(request):
@@ -9,6 +10,10 @@ def hello_world(request):
     if request.method == 'POST':
         input_data = request.POST.get('input_data')
 
-        return render(request, 'accountapp/hello_world.html', context={'message': input_data})
+        new_model = NewModel()
+        new_model.text = input_data
+        new_model.save()
+
+        return render(request, 'accountapp/hello_world.html', context={'new_model': new_model})
 
     return render(request, 'accountapp/hello_world.html')
