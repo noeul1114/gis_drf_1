b77bbd8f9ed261f521523e5478c8e036ac31170a
noeul1114 <parkhyongsok@naver.com>
2021-12-10 14:02:00 +0900

    Refresh error fixed

--- accountapp/views.py
+++ accountapp/views.py
@@ -1,7 +1,9 @@
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import render
 
 # Create your views here.
+from django.urls import reverse
+
 from accountapp.models import NewModel
 
 
@@ -14,10 +16,7 @@ def hello_world(request):
         new_model.text = input_data
         new_model.save()
 
-        new_model_list = NewModel.objects.all()
-        return render(request, 'accountapp/hello_world.html',
-                      context={'new_model': new_model,
-                               'new_model_list': new_model_list})
+        return HttpResponseRedirect(reverse('accountapp:hello_world'))
 
     new_model_list = NewModel.objects.all()
     return render(request, 'accountapp/hello_world.html',
