6d10eb35805b520ea2e115f0d07ea4c1155f92eb
noeul1114 <parkhyongsok@naver.com>
2021-12-15 11:11:28 +0900

    Profileapp create view update

--- profileapp/models.py
+++ profileapp/models.py
@@ -1,3 +1,14 @@
+from django.contrib.auth.models import User
 from django.db import models
 
 # Create your models here.
+
+
+class Profile(models.Model):
+    owner = models.OneToOneField(User, related_name='profile', on_delete=models.CASCADE)
+
+    nickname = models.CharField(max_length=50, null=False)
+    image = models.ImageField(upload_to='profile/', null=True, blank=True)
+    message = models.CharField(max_length=255, null=True, blank=True)
+
+    created_at = models.DateTimeField(auto_now_add=True)
--- profileapp/views.py
+++ profileapp/views.py
@@ -1,3 +1,17 @@
 from django.shortcuts import render
 
 # Create your views here.
+from django.urls import reverse
+from django.views.generic import CreateView
+
+from profileapp.forms import ProfileCreationForm
+from profileapp.models import Profile
+
+
+class ProfileCreateView(CreateView):
+    model = Profile
+    form_class = ProfileCreationForm
+    template_name = 'profileapp/create.html'
+
+    def get_success_url(self):
+        return reverse('accountapp:detail', kwargs={'pk': self.object.owner})
