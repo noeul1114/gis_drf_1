a5a9f8c7e7dd010b2035e25355073957b321d448
noeul1114 <parkhyongsok@naver.com>
2021-12-15 11:51:31 +0900

    Profile signal update

--- profileapp/models.py
+++ profileapp/models.py
@@ -2,6 +2,8 @@ from django.contrib.auth.models import User
 from django.db import models
 
 # Create your models here.
+from django.db.models.signals import post_save
+from django.dispatch import receiver
 
 
 class Profile(models.Model):
@@ -12,3 +14,9 @@ class Profile(models.Model):
     message = models.CharField(max_length=255, null=True, blank=True)
 
     created_at = models.DateTimeField(auto_now_add=True)
+
+
+@receiver(post_save, sender=User)
+def create_profile(sender, instance=None, created=False, **kwargs):
+    if created:
+        Profile.objects.create(owner=instance, nickname='임시 닉네임')
