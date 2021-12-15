ac04759a707a7381cfed9d90396c969f39482a7d
noeul1114 <parkhyongsok@naver.com>
2021-12-15 10:55:20 +0900

    Profileapp created

--- gis_drf_1/settings.py
+++ gis_drf_1/settings.py
@@ -56,6 +56,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'bootstrap5',
     'accountapp',
+    'profileapp',
 ]
 
 MIDDLEWARE = [
--- gis_drf_1/urls.py
+++ gis_drf_1/urls.py
@@ -20,4 +20,5 @@ urlpatterns = [
     path('admin/', admin.site.urls),
 
     path('accounts/', include('accountapp.urls')),
+    path('profiles/', include('profileapp.urls')),
 ]
