4a2829394e7ac4efc4abe05b78e471d8d2d32eb0
noeul1114 <parkhyongsok@naver.com>
2021-12-16 17:16:05 +0900

    Article app create view update

--- articleapp/urls.py
+++ articleapp/urls.py
@@ -1,5 +1,9 @@
+from django.urls import path
 
+from articleapp.views import ArticleCreateView
+
+app_name = 'articleapp'
 
 urlpatterns = [
-
+    path('create/', ArticleCreateView.as_view(), name='create'),
 ]
--- articleapp/views.py
+++ articleapp/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
 
 # Create your views here.
+from django.urls import reverse_lazy
+from django.views.generic import CreateView
+
+from articleapp.forms import ArticleCreationForm
+from articleapp.models import Article
+
+
+class ArticleCreateView(CreateView):
+    model = Article
+    form_class = ArticleCreationForm
+    success_url = reverse_lazy('accountapp:hello_world')
+    template_name = 'articleapp/create.html'
