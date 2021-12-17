228d111518ea2cd47b367269e919a942e366a93d
noeul1114 <parkhyongsok@naver.com>
2021-12-17 14:57:01 +0900

    Article Update / Delete view template update

--- articleapp/urls.py
+++ articleapp/urls.py
@@ -1,10 +1,12 @@
 from django.urls import path
 
-from articleapp.views import ArticleCreateView, ArticleDetailView
+from articleapp.views import ArticleCreateView, ArticleDetailView, ArticleUpdateView, ArticleDeleteView
 
 app_name = 'articleapp'
 
 urlpatterns = [
     path('create/', ArticleCreateView.as_view(), name='create'),
     path('detail/<int:pk>', ArticleDetailView.as_view(), name='detail'),
+    path('update/<int:pk>', ArticleUpdateView.as_view(), name='update'),
+    path('delete/<int:pk>', ArticleDeleteView.as_view(), name='delete'),
 ]
