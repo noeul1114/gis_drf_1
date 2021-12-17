2f9feb54d2603380550f1956cf6869ba4004e56c
noeul1114 <parkhyongsok@naver.com>
2021-12-17 11:30:40 +0900

    Article Detail View / Article thumbnail generation update

--- articleapp/models.py
+++ articleapp/models.py
@@ -1,7 +1,11 @@
+import sys
+
 from django.contrib.auth.models import User
+from django.core.files.uploadedfile import InMemoryUploadedFile
 from django.db import models
 
 # Create your models here.
+from image_processing import thumbnail
 
 
 class Article(models.Model):
@@ -14,3 +18,13 @@ class Article(models.Model):
     content = models.TextField(null=True)
 
     created_at = models.DateTimeField(auto_now_add=True)
+
+    def save(self, *args, **kwargs):
+        self.generate_thumbnail()
+        super().save(*args, **kwargs)
+
+    def generate_thumbnail(self):
+        if self.image:
+            output = thumbnail.generate_thumbnail(self.image)
+            self.thumb = InMemoryUploadedFile(output, 'ImageField', self.image.name,
+                                              'image/jpeg', sys.getsizeof(output), None)
--- articleapp/urls.py
+++ articleapp/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
 
-from articleapp.views import ArticleCreateView
+from articleapp.views import ArticleCreateView, ArticleDetailView
 
 app_name = 'articleapp'
 
 urlpatterns = [
     path('create/', ArticleCreateView.as_view(), name='create'),
+    path('detail/<int:pk>', ArticleDetailView.as_view(), name='detail'),
 ]
